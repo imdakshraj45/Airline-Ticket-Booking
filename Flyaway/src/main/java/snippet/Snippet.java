368b546a5d65f098694cb48cb3075671e4b96a93
Daksh Raj <darshrajsinghsolanki.dss@gmail.com>
2020-09-23 01:06:55 +0530

    Phase 2 project Send

---
+++ Flyaway/src/main/java/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	• An admin login page where the admin can change the password after login as
+	per their wish
+	• A master list of places for source and destination
+	• A master list of airlines
+	• A list of flights where each flight has a source, destination, airline, and ticket price
+}
+
